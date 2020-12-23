620e99314e154d3548c54e77ced6759804c6e75e
leicong <2920953443@qq.com>
2020-12-23 23:11:37 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <el-button type="primary" icon="el-icon-delete"></el-button>
 <el-button type="primary" icon="el-icon-search">搜索</el-button>
 <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+<p>这是一个新的内容，用来测试GitHub合并</p>
   </div>
 </template>
 
